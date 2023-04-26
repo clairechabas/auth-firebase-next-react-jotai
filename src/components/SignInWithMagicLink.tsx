8b56925c046bef34535ae8af5c4ff214e2506658
Claire Chabas <claire.chabas@gmail.com>
2023-04-26 21:39:04 +0200

    feat: sign in with magic link

---
+++ src/components/SignInWithMagicLink.tsx
@@ -0,0 +1,75 @@
+import { auth } from '@/firebase/app'
+import { authModalState } from '@/store/authModalAtom'
+import { Button, Flex, Input, Text } from '@chakra-ui/react'
+import { useSetAtom } from 'jotai'
+import React, { useState } from 'react'
+import { useSendSignInLinkToEmail } from 'react-firebase-hooks/auth'
+
+const SignInWithMagicLink: React.FC = () => {
+  const [sendSignInLinkToEmail, sending, fbError] =
+    useSendSignInLinkToEmail(auth)
+  const [email, setEmail] = useState('')
+  const [isSuccess, setIsSuccess] = useState(false)
+  const setAuthModalState = useSetAtom(authModalState)
+
+  const actionCodeSettings = {
+    url: 'http://localhost:3000/sign-in-with-link',
+    handleCodeInApp: true,
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    await sendSignInLinkToEmail(email, actionCodeSettings)
+
+    window.localStorage.setItem('emailForSignIn', email)
+
+    setIsSuccess(true)
+  }
+
+  return (
+    <Flex direction="column" w="full">
+      {isSuccess ? (
+        <>
+          <Text mt={3}>{`Check your email :)`}</Text>
+          <Button
+            variant="solid"
+            mt={5}
+            onClick={() =>
+              setAuthModalState((prev) => ({ ...prev, open: false }))
+            }
+          >
+            Ok
+          </Button>
+        </>
+      ) : (
+        <>
+          <Text>
+            {`Enter the email associated with your account and we'll send you a
+             link to log in.`}
+          </Text>
+
+          <form onSubmit={handleSubmit}>
+            <Input
+              required
+              type="email"
+              name="email"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+              mt={3}
+            />
+            <Button
+              type="submit"
+              isLoading={sending}
+              mt={3}
+              bg={sending ? 'primary.80' : 'primary.100'}
+            >
+              Send Link To Sign-In
+            </Button>
+          </form>
+        </>
+      )}
+    </Flex>
+  )
+}
+export default SignInWithMagicLink
