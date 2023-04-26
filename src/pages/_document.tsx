581ec4b1d9812e0e844d1bc1bb07d5a9afd2ac45
Claire Chabas <claire.chabas@gmail.com>
2023-04-26 22:13:24 +0200

    fix: display favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="images/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
