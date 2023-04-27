41091a3a4af2eadaa206c3623225a20095e9da3f
Claire Chabas <claire.chabas@gmail.com>
2023-04-27 16:40:54 +0200

    fix: fixing favicon path

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="favicon.png" />
+        <link rel="shortcut icon" href="images/favicon.png" />
       </Head>
       <body>
         <Main />
