1876d42fe47d1e35b4efcf3cfa13da8e979c5f61
Claire Chabas <claire.chabas@gmail.com>
2023-04-27 15:15:54 +0200

    feat: sign in with link landing page styles and handling form submit

--- src/pages/sign-in-with-link.tsx
+++ src/pages/sign-in-with-link.tsx
@@ -1,19 +1,31 @@
 import { auth } from '@/firebase/app'
-import { Button, Flex, Input, Text } from '@chakra-ui/react'
+import {
+  Button,
+  Flex,
+  FormControl,
+  Heading,
+  Image,
+  Input,
+  Text,
+} from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { useSignInWithEmailLink } from 'react-firebase-hooks/auth'
 
 const SignInWithLinkPage: React.FC = () => {
-  const [signInWithEmailLink, user, loading, fbError] =
-    useSignInWithEmailLink(auth)
+  const [signInWithEmailLink, user, loading] = useSignInWithEmailLink(auth)
   const [email, setEmail] = useState('')
   const router = useRouter()
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    signInAndRedirect()
+    // signInAndRedirect()
+    await signInWithEmailLink(email, window.location.href)
+
+    window.localStorage.removeItem('emailForSignIn')
+    // router.push('/')
+    console.log('signed in!')
   }
 
   const signInAndRedirect = async () => {
@@ -25,46 +37,67 @@ const SignInWithLinkPage: React.FC = () => {
 
   useEffect(() => {
     const emailFromStorage = window.localStorage.getItem('emailForSignIn')
-
-    if (emailFromStorage) {
-      setEmail(emailFromStorage)
-      signInAndRedirect()
-    } else {
-      setEmail('')
-    }
+    console.log('emailFromStorage', emailFromStorage)
+    // if (emailFromStorage) {
+    //   setEmail(emailFromStorage)
+    //   signInAndRedirect()
+    // } else {
+    //   setEmail('')
+    // }
   }, [])
 
   return (
-    <Flex>
-      <Text>Signing In With Magic Link</Text>
-      {email ? (
-        <Text>{`Wait while we're authenticating you...`}</Text>
-      ) : (
-        <>
-          <Text>
-            {`Please, confirm the email you used to request this sign-in link
-             so we can confirm your identity.`}
-          </Text>
+    <Flex
+      direction="column"
+      align="center"
+      position="relative"
+      top={200}
+      text-align="center"
+      width={{ base: '80%', md: '60%', lg: '600px' }}
+      height="100vh"
+      mx="auto"
+    >
+      <Flex w="full" direction="column" align="stretch" textAlign="center">
+        <Image
+          width="90px"
+          alignSelf="center"
+          src="images/react-firebase.svg"
+          alt="Firebase React Logo"
+        />
+        <Heading fontSize="20pt" fontWeight={700} mt={5}>
+          Signing In With Magic Link
+        </Heading>
 
-          <form onSubmit={handleSubmit}>
-            <Input
-              required
-              type="email"
-              name="email"
-              placeholder="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {fbError && (
-              <Text textAlign="center" color="red" fontSize="10pt">
-                {fbError.message}
-              </Text>
-            )}
-            <Button type="submit" isLoading={loading}>
-              Confirm Email & Log In
-            </Button>
-          </form>
-        </>
-      )}
+        {email ? (
+          <Text
+            fontSize="12pt"
+            mt={16}
+          >{`Wait while we're authenticating you...`}</Text>
+        ) : (
+          <>
+            <Text fontSize="12pt" mt={16}>
+              {`Please, confirm the email you used to request this sign-in link
+             so we can confirm your identity.`}
+            </Text>
+
+            <form onSubmit={handleSubmit}>
+              <FormControl isRequired width="500px" mx="auto">
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Enter email used to sign-in..."
+                  size="md"
+                  mt={8}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Button type="submit" mt={5} isLoading={loading}>
+                  Confirm Email & Log In
+                </Button>
+              </FormControl>
+            </form>
+          </>
+        )}
+      </Flex>
     </Flex>
   )
 }
