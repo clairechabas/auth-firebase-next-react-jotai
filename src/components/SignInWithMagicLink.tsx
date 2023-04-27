17ba8116944070615416a0da8c498e4655f85574
Claire Chabas <claire.chabas@gmail.com>
2023-04-27 12:36:12 +0200

    feat: udpate url for magic link sign in

--- src/components/SignInWithMagicLink.tsx
+++ src/components/SignInWithMagicLink.tsx
@@ -13,7 +13,7 @@ const SignInWithMagicLink: React.FC = () => {
   const setAuthModalState = useSetAtom(authModalState)
 
   const actionCodeSettings = {
-    url: 'http://localhost:3000/sign-in-with-link',
+    url: 'https://auth-firebase-next-react-jotai-hsl8duqgb-clairechabas.vercel.app/sign-in-with-link',
     handleCodeInApp: true,
   }
 
